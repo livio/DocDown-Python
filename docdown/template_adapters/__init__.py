8184a1231ff81250fcd23ebd98331b615e3c2498
Justin Michalicek <jmichalicek@gmail.com>
2016-10-25 13:57:01 -0400

    Rename docdown.template_adapters.pystache_adapter
    * Renamed docdown.template_adapters.pystache_adapter to
      docdown.template_adapters.pystache for consistency, shorter name,
      and removal of redundant info in names

--- docdown/template_adapters/__init__.py
+++ docdown/template_adapters/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import absolute_import, unicode_literals, print_function
 
-from .pystache_adapter import PystacheAdapter
+from .pystache import PystacheAdapter
 from .string_format import StringFormatAdapter
 from .template_string import TemplateStringAdapter
--- tests/template_adapters/test_pystache.py
+++ tests/template_adapters/test_pystache.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-test_pystache_adapter
+test_pystache
 ----------------------------------
 
-Tests for `docdown.template_adapters.pystache_adapter` module.
+Tests for `docdown.template_adapters.pystache` module.
 """
 
 from __future__ import absolute_import, unicode_literals, print_function
 
 import unittest
 
-from docdown.template_adapters import pystache_adapter
+from docdown.template_adapters import pystache
 
 
 class PystacheAdapterTest(unittest.TestCase):
@@ -19,7 +19,7 @@ class PystacheAdapterTest(unittest.TestCase):
     def test_render(self):
         template = 'Hi, my name is {{ name }}'
         context = {'name': 'Justin'}
-        adapter = pystache_adapter.PystacheAdapter()
+        adapter = pystache.PystacheAdapter()
         result = adapter.render(template, context)
 
         expected = 'Hi, my name is Justin'
@@ -27,7 +27,7 @@ class PystacheAdapterTest(unittest.TestCase):
 
     def test_render_with_default_context(self):
         template = 'Hi, my name is Justin'
-        adapter = pystache_adapter.PystacheAdapter()
+        adapter = pystache.PystacheAdapter()
         result = adapter.render(template)
 
         expected = 'Hi, my name is Justin'
