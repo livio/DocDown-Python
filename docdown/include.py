740886109c52edc1bbdcc7faf37aa2e4bce7b07e
Justin Michalicek <jmichalicek@gmail.com>
2016-11-01 14:46:44 -0400

    Fix up include extension
    * Correct file path handling
    * Break out csv to html table into its own method for easier unit testing
    * Align config params with javascript docdown lib

--- docdown/include.py
+++ docdown/include.py
@@ -12,22 +12,34 @@ import unicodecsv
 
 class IncludePreprocessor(Preprocessor):
 
-    def __init__(self, root_directory=None, asset_directory=None, extension_map=None, **kwargs):
+    def __init__(self, root_directory='', current_directory='', asset_directory='', extension_map=None, **kwargs):
         self.asset_directory = asset_directory
+        self.current_directory = current_directory
         self.root_directory = root_directory
         self.extension_map = extension_map or {}
         super(IncludePreprocessor, self).__init__(**kwargs)
 
     def find_file_path(self, file_name):
-        asset_directory = self.asset_directory
-        file_path = os.path.join(asset_directory, file_name)
-        while not os.path.isfile(file_path):
-            if asset_directory == self.root_directory:
-                return None
-            head, tail = os.path.split(os.path.split(asset_directory)[0])
-            asset_directory = os.path.join(head, 'assets')
-            file_path = os.path.join(asset_directory, file_name)
+        """
+        Find path to the file within self.root_directory
 
+        Start at `self.current_directory/self.asset_directory/file_name` looking for `file_name`.  Recursively
+        strip one directory level off of `self.current_directory` to see if `self.asset_directory/file_name`
+        exists there up until the path matched `self.root_directory`
+        """
+        current_dir = self.current_directory
+        asset_directory = os.path.join(current_dir, self.asset_directory)
+        file_path = os.path.join(asset_directory, file_name)
+
+        root_directory = os.path.join(self.root_directory, self.asset_directory)
+
+        while not os.path.isfile(file_path):
+            if asset_directory == root_directory:
+                return None
+
+            head, tail = os.path.split(os.path.split(asset_directory)[0])
+            asset_directory = os.path.join(head, self.asset_directory)
+            file_path = os.path.join(asset_directory, file_name)
         return file_path
 
     def handle_code(self, file_path, file_extension):
@@ -48,10 +60,7 @@ class IncludePreprocessor(Preprocessor):
         included_file.close()
         return md_lines
 
-    def handle_csv(self, file_path):
-        """
-        Parse csv file and return as an html table
-        """
+    def build_csv_table(self, file_path):
         table_html = ['<table>']
         with open(file_path, 'rb') as csvfile:
             reader = unicodecsv.reader(csvfile)
@@ -63,8 +72,13 @@ class IncludePreprocessor(Preprocessor):
                     table_html.append('</th>' if index == 0 else '</td>')
                 table_html.append('</tr>')
         table_html.append('</table>')
+        return table_html
 
-        return self.markdown.htmlStash.store(''.join(table_html), safe=True)
+    def handle_csv(self, file_path):
+        """
+        Parse csv file and return as an html table
+        """
+        return self.markdown.htmlStash.store(''.join(self.build_csv_table(file_path)), safe=True)
 
     def run(self, lines):
         md_lines = []
@@ -72,6 +86,8 @@ class IncludePreprocessor(Preprocessor):
             if line.startswith('+++'):
                 file_name = line[3:].strip()
                 file_path = self.find_file_path(file_name)
+                if not file_path:
+                    break
                 file_extension = os.path.splitext(file_name)[1]
                 if file_extension == '.csv':
                     md_lines.append(self.handle_csv(file_path))
@@ -86,7 +102,9 @@ class IncludeExtension(Extension):
 
     def __init__(self, **kwargs):
         self.config = {
-            'asset_directory': ['', 'Directory for the assets to include via this extension'],
+            'asset_directory': ['', ('Name of the dirirectory for the assets to include via this extension within'
+                                     ' current_directory and root_directory')],
+            'current_directory': ['', 'Root directory to stop searching for assets'],
             'root_directory': ['', 'Root directory to stop searching for assets'],
             'extension_map': [{}, ('Optional dictionary mapping one file extension to another'
                                    ' to override the default assigned by markdown.extensions.fenced_code')]
@@ -99,11 +117,13 @@ class IncludeExtension(Extension):
 
         asset_directory = self.getConfig('asset_directory')
         root_directory = self.getConfig('root_directory')
+        current_directory = self.getConfig('current_directory')
         extension_map = self.getConfig('extension_map')
 
         md.preprocessors.add(
             'include',
             IncludePreprocessor(root_directory=root_directory,
+                                current_directory=current_directory,
                                 asset_directory=asset_directory,
                                 extension_map=extension_map,
                                 markdown_instance=md),
