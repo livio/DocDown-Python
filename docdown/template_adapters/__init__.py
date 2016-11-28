59a517926ed84fc2d3e05f41d47ec66ec3a9a57c
Justin Michalicek <jmichalicek@gmail.com>
2016-11-28 13:06:33 -0500

    Stop importing PystacheAdapter automatically
    * Pystache is an optional dependency.  Importing PystacheAdapter into
      __init__.py makes it not optional.

--- docdown/template_adapters/__init__.py
+++ docdown/template_adapters/__init__.py
@@ -2,6 +2,5 @@
 
 from __future__ import absolute_import, unicode_literals, print_function
 
-from .pystache import PystacheAdapter
 from .string_format import StringFormatAdapter
 from .template_string import TemplateStringAdapter
