e285d9e95712d268af948bfbc49f14391881c8f1
Jason Emerick <jemerick@gmail.com>
2019-06-05 18:43:37 -0400

    bump version to 0.2.2

--- docdown/__init__.py
+++ docdown/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Jason Emerick"""
 __email__ = 'jason@mobelux.com'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='docdown',
-    version='0.2.1',
+    version='0.2.2',
     description="DocDown is a Markdown extension for source code documentation.",
     long_description=readme + '\n\n' + history,
     author="Jason Emerick, Justin Michalicek",
